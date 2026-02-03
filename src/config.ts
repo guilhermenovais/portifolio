45bd4f3d9fc6a324e34005faeabbd58090186916
Guilherme Novais <guilhermens@ufmg.com>
2026-02-03 18:57:18 -0300

    chore: initialize project

---
+++ src/config.ts
@@ -0,0 +1,87 @@
+export const config = {
+  name: 'Guilherme Novais',
+  email: 'guilhermenovaisdesouza@proton.me',
+  linkedin: 'https://linkedin.com/in/guilhermenovaisdesouza',
+  github: 'https://github.com/guilhermenovais',
+  whatsapp: 'https://wa.me/5531975747435',
+  resumeEn: '/cv-guilherme-novais-de-souza-en.pdf',
+  resumePt: '/cv-guilherme-novais-de-souza-pt.pdf',
+};
+
+export const projects = [
+  {
+    id: 1,
+    title: {
+      en: 'SISPEQ',
+      pt: 'SISPEQ'
+    },
+    description: {
+      en: 'A system for generating quality control reports for oil refineries',
+      pt: 'Um sistema para gerar relatórios de controle de qualidade para refinarias de petróleo'
+    },
+    image: '/project1.jpg',
+    github: 'https://github.com/guilhermenovais/sispeq-server'
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    title: {
+      en: 'Backend Developer',
+      pt: 'Desenvolvedor Backend'
+    },
+    company: 'Segware',
+    period: {
+      en: 'Sep 2024 - Present',
+      pt: 'Set 2024 - Presente'
+    },
+    description: {
+      en: 'Development of alarm and access control management systems, with a focus on equipment integrations and platform architectural improvements.',
+      pt: 'Desenvolvimento de sistemas de gerenciamento de alarmes e controle de acesso, com foco em integrações com equipamentos e melhoria arquitetural da plataforma.'
+    }
+  },
+  {
+    id: 2,
+    title: {
+      en: 'Fullstack Developer',
+      pt: 'Desenvolvedor Fullstack'
+    },
+    company: 'AGSinn',
+    period: {
+      en: 'May 2022 - Sep 2024',
+      pt: 'Mai 2022 - Set 2024'
+    },
+    description: {
+      en: 'Development of solutions tailored for businesses, with a focus on industrial access control.',
+      pt: 'Desenvolvimento de soluções direcionadas a empresas, com ênfase no controle de acesso a ambientes industriais.'
+    }
+  },
+];
+
+export const education = [
+  {
+    id: 1,
+    course: {
+      en: 'Bachelor of Information Systems',
+      pt: 'Bacharelado em Sistemas de Informação'
+    },
+    institution: 'UFMG',
+    period: {
+      en: '2022 - Present',
+      pt: '2022 - Presente'
+    }
+  },
+  {
+    id: 2,
+    course: {
+      en: 'Technical Course in Electronics',
+      pt: 'Curso Técnico em Eletrônica'
+    },
+    institution: 'CEFET-MG',
+    period: {
+      en: '2019 - 2021',
+      pt: '2019 - 2021'
+    }
+  },
+];
